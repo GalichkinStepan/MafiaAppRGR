629260f9f8608a077d8ff792f0dcb35a6fc137c2
Stepan <s_galichkin@mail.ru>
2023-04-02 19:53:10 +0700

    Добавьте файлы проекта.

---
+++ MafiaAppRGR/MafiaAppRGR/Pages/GamePages/FirstNightPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MafiaAppRGR.Pages.GamePages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FirstNightPage : ContentPage
+    {
+        public FirstNightPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
