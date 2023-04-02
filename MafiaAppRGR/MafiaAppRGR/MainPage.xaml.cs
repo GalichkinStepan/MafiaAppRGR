48a938808cb995d932635390f8b0d55dd267a9cd
Stepan <s_galichkin@mail.ru>
2023-04-02 19:55:47 +0700

    Первый коммит, структура проекта

--- MafiaAppRGR/MafiaAppRGR/MainPage.xaml.cs
+++ MafiaAppRGR/MafiaAppRGR/MainPage.xaml.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
-using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+﻿using Xamarin.Forms;
+
 
 namespace MafiaAppRGR
 {
